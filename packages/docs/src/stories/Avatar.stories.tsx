58f7b6ec354086f3d2edebc2ab74b0c36afd567b
Yuri Maciel <yurimpro@outlook.com>
2024-01-29 10:22:09 -0300

    add avatar story

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,18 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@yurimaciel-ui/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/o-yurimaciel.png',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: 'undefined',
+  },
+}

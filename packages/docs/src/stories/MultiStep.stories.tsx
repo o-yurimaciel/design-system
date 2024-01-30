5209f7795edc91d02cb7a3f35fdd39dab78bfd02
Yuri Maciel <yurimpro@outlook.com>
2024-01-30 09:39:04 -0300

    add multi step component & story

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { MultiStep, MultiStepProps } from '@yurimaciel-ui/react'
+
+export default {
+  title: 'Form/MultiStep',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
